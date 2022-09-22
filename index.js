58247d0a4bb8320a1767100efdafbb68599753fc
LalaChristl <lesleyannchristl@gmail.com>
2022-09-22 14:58:01 +0200

    edit

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-// const body = document.querySelector('body')
-// const button = document.querySelector("button");
 const popup = document.querySelector(".container");
-// const close = document.querySelector(".pop-up-close");
 
-popup.style.display = 'none';
+popup.style.display = "none";
 
-// button.addEventListener("click", () => {
-//   popup.style.display = "block";
-// });
-
-// close.addEventListener("click", ()=> {
-//     popup.style.display ="none"
-// })
-
-// popup.addEventListener("click", () => {
-//     popup.style.display="none";
-// })
-
-
-
-
-
-document.addEventListener('mousemove', (e) => {
-    if(e.clientY >10) {
-        popup.style.display = 'block';
-    }
-})
+document.addEventListener("mousemove", (e) => {
+  if (e.clientY > 10) {
+    popup.style.display = "block";
+  }
+});
